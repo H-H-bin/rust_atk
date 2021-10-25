b35528657f011f6fa5c4aa4a8ae60f3dd079a877
Bin.H <H-H-bin@163.com>
2021-10-26 01:32:22 +0800

    Test Port Read/Write
    Signed-off-by: Bin.H <H-H-bin@163.com>

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
-
 use serde::{Deserialize, Serialize};
-use serialport::{available_ports, SerialPortType};
+use serialport::{available_ports, DataBits, SerialPortType, StopBits};
+use std::io::{self, Write};
+use std::time::Duration;
 use wmi::{COMLibrary, WMIConnection, WMIError};
 
 #[cfg(windows)]
@@ -94,4 +95,54 @@ fn main() {
             eprintln!("Error: {}", e);
         }
     };
+
+    // 1. Get port name
+    //TODO: if there only one port, then just open it, if there more then one port,
+    //      pop up for user to chose one.
+
+    let modem_port = get_modem_ports_and_return_vec_struct().unwrap();
+    let port_name = &modem_port[0].AttachedTo;
+    let baud_rate = 115200;
+
+    // 2. Open Port
+    let builder = serialport::new(port_name, baud_rate)
+        .stop_bits(StopBits::One)
+        .data_bits(DataBits::Eight)
+        .timeout(Duration::from_millis(10));
+    println!("{:?}", &builder);
+
+    //3. Write to port
+    let msg = "ATI\r\n";
+    let mut port = builder.open().unwrap_or_else(|e| {
+        eprintln!("Failed to open \"{}\". Error: {}", port_name, e);
+        ::std::process::exit(1);
+    });
+
+    println!(
+        "Writing {} to {} at {} baud",
+        &msg, &port_name, &baud_rate
+    );
+
+    match port.write(&msg.as_bytes()) {
+        Ok(_) => {
+            print!("{}", &msg);
+            std::io::stdout().flush().unwrap();
+        }
+        Err(ref e) if e.kind() == io::ErrorKind::TimedOut => (),
+        Err(e) => eprintln!("{:?}", e),
+    }
+
+    //4. Read data
+    let mut serial_buf: Vec<u8> = vec![0; 1000];
+    println!("Receiving data on {} at {} baud:", &port_name, &baud_rate);
+    match port.read(serial_buf.as_mut_slice()) {
+        Ok(t) => io::stdout().write_all(&serial_buf[..t]).unwrap(),
+        Err(ref e) if e.kind() == io::ErrorKind::TimedOut => (),
+        Err(e) => eprintln!("{:?}", e),
+    }
+
+    //TODO:Add a loop for data read/write
+    //TODO:Add promot like a shell
+    //TODO:Add auto complete with AT command history
+    //TODO:Colors
 }
